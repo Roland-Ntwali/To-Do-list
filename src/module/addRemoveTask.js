5dd2aa30dd173900ae9eca8b1a3e28cf8e65a910
Roland-Ntwali <rolandntwali@gmail.com>
2023-02-23 23:06:07 +0200

    Add function to clear all complete button and made to do list interactive

--- src/module/addRemoveTask.js
+++ src/module/addRemoveTask.js
@@ -2,8 +2,20 @@ const form = document.querySelector('form');
 const input = document.querySelector('.input');
 const todoContainer = document.querySelector('.todoContainer');
 
+let todos = [];
 let todo;
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+// function  saving todos to the local storage
+const saveTodo = () => {
+  const allTodos = JSON.stringify(todos);
+  localStorage.setItem('todos', allTodos);
+};
+
+// function getting stored thing from the storage
+const getStoredTodos = () => {
+  todos = JSON.parse(localStorage.getItem('todos'));
+};
+
 const store = () => {
   todo = {
     Description: input.value,
@@ -11,30 +23,55 @@ const store = () => {
     completed: false,
   };
   todos.push(todo);
-  localStorage.setItem('todos', JSON.stringify(todos));
+  saveTodo();
 };
 
+// function clearing the input field
 const clear = () => {
   input.value = '';
 };
 
+// stats stand for status
+const completedTodo = (stats, index) => {
+  todos[index - 1].completed = stats;
+  saveTodo();
+};
+
 const removeTask = (id) => {
-  todos = todos.filter((books) => books.id !== id);
+  todos = todos.filter((task) => task.id !== id);
   todos.forEach((todo, id) => {
     todo.id = id + 1;
   });
-  localStorage.setItem('todos', JSON.stringify(todos));
+  saveTodo();
 };
 
+// dynamic html with checkbox definition
 const addTask = (todo) => {
   const ul = document.createElement('div');
   const checkBox = document.createElement('input');
   checkBox.type = 'checkbox';
   checkBox.classList.add('checkBox');
+
   const newInp = document.createElement('input');
   newInp.type = 'text';
   newInp.classList.add('newInput');
   newInp.value = todo.Description;
+
+  checkBox.onclick = (e) => {
+    completedTodo(e.target.checked, todo.id);
+
+    if (todo.completed === true) {
+      newInp.classList.add('completed');
+    } else {
+      newInp.classList.remove('completed');
+    }
+  };
+
+  if (todo.completed === true) {
+    checkBox.checked = 'checked';
+    newInp.classList.add('completed');
+  }
+
   const icon = document.createElement('i');
   icon.classList.add('fa-solid');
   icon.classList.add('fa-trash');
@@ -56,7 +93,7 @@ const editTodoList = () => {
       todos.forEach((todo, index) => {
         if (indexy === index) {
           todo.Description = edits.value;
-          localStorage.setItem('todos', JSON.stringify(todos));
+          saveTodo();
         }
       });
     });
@@ -64,7 +101,7 @@ const editTodoList = () => {
 };
 editTodoList();
 
-function populate() {
+function formSubmission() {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (input.value !== '') {
@@ -75,4 +112,29 @@ function populate() {
   });
 }
 
-export { populate, editTodoList };
+const populateTasks = () => {
+  if (localStorage.getItem('todos')) {
+    getStoredTodos();
+    todos.map((task) => {
+      addTask(task);
+      return task;
+    });
+  } else {
+    todos.map((task) => {
+      addTask(task);
+      return task;
+    });
+  }
+};
+
+const clearCompleted = () => {
+  todos = todos.filter((task) => task.completed !== true);
+  todos.forEach((todo, id) => {
+    todo.id = id + 1;
+  });
+  saveTodo();
+};
+
+export {
+  formSubmission, editTodoList, populateTasks, clearCompleted,
+};
