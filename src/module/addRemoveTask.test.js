4e06680cc5072946a90d9a2c60b649a931402979
Walid Kabboussa <walidkabou@gmail.com>
2023-03-01 16:21:30 +0100

    Fix webpack and jsdom

---
+++ src/module/addRemoveTask.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-expressions */
+import { addTask, removeTask } from './addRemoveTask.js';
+
+describe('Ading and Removing a function', () => {
+  test('adding to do task', () => {
+    expect(addTask).not.toBeNull();
+  });
+
+  test('remove a task task', () => {
+    expect(removeTask).not.toBeDefined;
+  });
+  test('remove a task task', () => {
+    expect(removeTask.parentElement).not.toBe('');
+  });
+});
