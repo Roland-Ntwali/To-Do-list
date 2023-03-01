d89fa704997bc163ef25af9680ebf6b881d38fb4
Walid Kabboussa <walidkabou@gmail.com>
2023-03-01 18:25:13 +0100

    Fix linters

--- src/module/addRemoveTask.test.js
+++ src/module/addRemoveTask.test.js
@@ -6,7 +6,7 @@ describe('Adding and Removing a function', () => {
   });
 
   test('remove a task task', () => {
-    expect(removeTask).not.toBeDefined;
+    expect(removeTask).not.toBe('');
   });
   test('remove a task task', () => {
     expect(removeTask.parentElement).not.toBe('');
