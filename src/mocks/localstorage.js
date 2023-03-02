f1dea82ee1562ae62f0e87c2d1964d8c6a767461
Roland-Ntwali <rolandntwali@gmail.com>
2023-03-02 11:03:43 +0200

    Refactored code and fix webpack

---
+++ src/mocks/localstorage.js
@@ -0,0 +1,19 @@
+const mockItems = [
+    {
+      description: 'eat',
+      id: 1,
+      completed: false,
+    },
+    {
+      description: 'drink',
+      id: 1,
+      completed: false,
+    },
+    {
+      description: 'code',
+      id: 1,
+      completed: false,
+    },
+  ];
+  
+  export default mockItems;
